90b6d5c1fcfe85fc07d43d1c30cfdc40452c6eca
Elijay <laura.merris@googlemail.com>
2017-04-07 12:23:37 +0100

    add canvas js and styles

--- init.js
+++ init.js
@@ -1,3 +1,10 @@
 (function(){
-  alert('hello world');
+
+// main setup javascript goes here.
+
+var canvas-main = document.getElementById("canvas"),
+canvas-msg = document.getElementById("msg"),
+context-main = canvas-main.getContext("2d"),
+context-msg = canvas-msg.getContext("2d");
+
 })();
